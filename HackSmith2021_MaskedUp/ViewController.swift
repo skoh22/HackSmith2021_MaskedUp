70c6988a31d225afc71d6230521513bd3eca0bd1
Sophie Koh <skoh22@amherst.edu>
2021-03-26 17:38:52 -0400

    Initial Commit

---
+++ HackSmith2021_MaskedUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackSmith2021_MaskedUp
+//
+//  Created by Sophie Koh on 3/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
