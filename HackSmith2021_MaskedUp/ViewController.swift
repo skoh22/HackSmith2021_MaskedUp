6f1868ec07e40e4104e410ca76ef295e4468fc42
Sophie Koh <skoh22@amherst.edu>
2021-03-27 02:04:19 -0400

    image scaling and app icon

--- HackSmith2021_MaskedUp/ViewController.swift
+++ HackSmith2021_MaskedUp/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var butt:UIButton!
     var buttonType:Bool = false //false means it is ready to take a photo
     var imagePicker: UIImagePickerController!
+    var uiImage: UIImage? = nil
 
     // photo code adapted from https://www.ioscreator.com/tutorials/take-photo-ios-tutorial
     
@@ -30,19 +31,18 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             maskClass?.isHidden = true
             
             //take photo
-            imagePicker =  UIImagePickerController()
+            imagePicker = UIImagePickerController()
             imagePicker.delegate = self
             imagePicker.sourceType = .camera
             imagePicker.cameraFlashMode = .off
             imagePicker.allowsEditing = true
             present(imagePicker, animated: true, completion: nil)
-            
             //change button
             butt.setTitle("Classify", for: .normal)
             buttonType=true
         }
         else{
-            let maskStat:Int = classify(imagePicker: imagePicker)
+            let maskStat:Int = classify()
             if maskStat == 0{
                 maskClass?.textColor = UIColor.green
                 maskClass?.text = "Correct"
@@ -72,15 +72,32 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             imagePicker.dismiss(animated: true, completion: nil)
-        imageView?.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+            uiImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+            imageView?.image = uiImage
         }
-    func classify(imagePicker:UIImagePickerController)-> Int{
+    func classify()-> Int{
         var maskStatus:Int
+        let scaledImage = scaleToSize(img:uiImage!)
+        let png:Data = scaledImage.pngData()!
         //Use ML model to figure this out but using random number for now
+        
         let randomInt = Int.random(in: 0..<4)
         maskStatus = randomInt
         //
         return maskStatus
     }
+    
+    func scaleToSize(img:UIImage) -> UIImage{
+        let size = img.size
+        let ratio  = 256.0/size.width
+        let newSize:CGSize = CGSize(width: size.width*ratio, height: size.height*ratio)
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        img.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+
 }
 
